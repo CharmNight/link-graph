c8a3098717bb9b43a513962981a4e27a1880e358
GL <499715548@qq.com>
2026-04-28 11:44:55 +0800

    test: replace local user paths in navigation tests

--- src/test/kotlin/com/charmnight/linkgraph/navigation/SourceNavigationPathResolverTest.kt
+++ src/test/kotlin/com/charmnight/linkgraph/navigation/SourceNavigationPathResolverTest.kt
@@ -6,49 +6,51 @@ import kotlin.test.assertEquals
 import kotlin.test.assertNull
 
 class SourceNavigationPathResolverTest {
+    private val testRoot = "/home/example/link-graph-test"
+
     @Test
     fun normalizesArchiveEntryPathsWithAndWithoutBang() {
         assertEquals(
-            "/home/example/link-graph-test/.m2/repository/demo.jar!/org/example/Demo.class",
+            "$testRoot/.m2/repository/demo.jar!/org/example/Demo.class",
             SourceNavigationPathResolver.normalizeArchiveEntryPath(
-                "/home/example/link-graph-test/.m2/repository/demo.jar/org/example/Demo.class",
+                "$testRoot/.m2/repository/demo.jar/org/example/Demo.class",
             ),
         )
         assertEquals(
-            "/home/example/link-graph-test/.jdks/zulu17/src.zip!/java/lang/ThreadLocal.java",
+            "$testRoot/jdks/zulu17/src.zip!/java/lang/ThreadLocal.java",
             SourceNavigationPathResolver.normalizeArchiveEntryPath(
-                "/home/example/link-graph-test/.jdks/zulu17/src.zip/java/lang/ThreadLocal.java",
+                "$testRoot/jdks/zulu17/src.zip/java/lang/ThreadLocal.java",
             ),
         )
         assertEquals(
-            "/home/example/link-graph-test/.m2/repository/demo.jar!/org/example/Demo.class",
+            "$testRoot/.m2/repository/demo.jar!/org/example/Demo.class",
             SourceNavigationPathResolver.normalizeArchiveEntryPath(
-                "/home/example/link-graph-test/.m2/repository/demo.jar!/org/example/Demo.class",
+                "$testRoot/.m2/repository/demo.jar!/org/example/Demo.class",
             ),
         )
-        assertNull(SourceNavigationPathResolver.normalizeArchiveEntryPath("/home/example/link-graph-test/demo.txt"))
+        assertNull(SourceNavigationPathResolver.normalizeArchiveEntryPath("$testRoot/demo.txt"))
     }
 
     @Test
     fun buildsJrtUrlsFromExplodedModulePaths() {
         assertEquals(
-            "${StandardFileSystems.JRT_PROTOCOL_PREFIX}/home/example/link-graph-test/jdks/zulu17!/java.base/java/lang/ThreadLocal.class",
+            "${StandardFileSystems.JRT_PROTOCOL_PREFIX}$testRoot/jdks/zulu17!/java.base/java/lang/ThreadLocal.class",
             SourceNavigationPathResolver.buildJrtUrl(
-                "/home/example/link-graph-test/jdks/zulu17/java.base/java/lang/ThreadLocal.class",
+                "$testRoot/jdks/zulu17/java.base/java/lang/ThreadLocal.class",
             ),
         )
         assertEquals(
-            "${StandardFileSystems.JRT_PROTOCOL_PREFIX}/home/example/link-graph-test/.jdks/zulu17!/java.base/java/lang/ThreadLocal.class",
+            "${StandardFileSystems.JRT_PROTOCOL_PREFIX}$testRoot/.jdks/zulu17!/java.base/java/lang/ThreadLocal.class",
             SourceNavigationPathResolver.buildJrtUrl(
-                "/home/example/link-graph-test/.jdks/zulu17/java.base/java/lang/ThreadLocal.class",
+                "$testRoot/.jdks/zulu17/java.base/java/lang/ThreadLocal.class",
             ),
         )
         assertEquals(
-            "${StandardFileSystems.JRT_PROTOCOL_PREFIX}/home/example/link-graph-test/jdks/zulu17!/java.base/java/lang/ThreadLocal.class",
+            "${StandardFileSystems.JRT_PROTOCOL_PREFIX}$testRoot/jdks/zulu17!/java.base/java/lang/ThreadLocal.class",
             SourceNavigationPathResolver.buildJrtUrl(
-                "/home/example/link-graph-test/jdks/zulu17!/java.base/java/lang/ThreadLocal.class",
+                "$testRoot/jdks/zulu17!/java.base/java/lang/ThreadLocal.class",
             ),
         )
-        assertNull(SourceNavigationPathResolver.buildJrtUrl("/home/example/link-graph-test/project/src/main/java/com/example/OrderService.java"))
+        assertNull(SourceNavigationPathResolver.buildJrtUrl("$testRoot/project/src/main/java/com/example/OrderService.java"))
     }
 }
